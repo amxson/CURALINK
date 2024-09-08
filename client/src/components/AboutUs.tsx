7987fb88f8e827b23df126a20771571c7d24672d
danielbirhanu <danielbir49@gmail.com>
2024-09-08 14:59:10 +0300

    Modify the text for the hero and about us section

--- client/src/components/AboutUs.tsx
+++ client/src/components/AboutUs.tsx
@@ -15,13 +15,9 @@ const AboutUs: React.FC = () => {
           </div>
           <div className="hero-content">
             <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Quibusdam tenetur doloremque molestias repellat minus asperiores
-              in aperiam dolor, quaerat praesentium. Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Voluptatibus, repudiandae! Lorem
-              ipsum dolor sit amet consectetur adipisicing elit. Provident
-              quibusdam doloremque ex? Officia atque ab dolore? Tempore totam
-              non ea!
+            At CuraLink, we are dedicated to bridging the gap between patients and healthcare providers through a reliable, user-friendly digital platform. Our mission is to make healthcare accessible, efficient, and personalized for everyone. By partnering with leading doctors, clinics, and hospitals, we ensure that you receive high-quality care whenever you need it. With a focus on patient satisfaction and innovative technology, CuraLink empowers you to take control of your health with ease.
+
+            Your health is our priority, and CuraLink is here to connect you with the care you deserve.
             </p>
           </div>
         </div>
--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -11,9 +11,7 @@ const Hero: React.FC = () => {
           Our Responsibility
         </h1>
         <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-          tenetur doloremque molestias repellat minus asperiores in aperiam
-          dolor, quaerat praesentium.
+        Welcome to CuraLink, your trusted online platform for seamless doctor appointments and healthcare management. At CuraLink, we understand the importance of easy access to healthcare, so we’ve created a system that connects you to the best healthcare professionals at your convenience. Whether you need a routine check-up, specialist consultation, or immediate care, CuraLink is here to simplify your healthcare journey.
         </p>
       </div>
       <div className="hero-img">
